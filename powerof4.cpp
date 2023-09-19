00654baa3d1c51ba1c0f5720332a453414ef4a46
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-09-19 15:17:02 +0300

    feat : solved one more

---
+++ powerof4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define BINARYHOLDER1 1431655765
+//this is 1010101010101010101010101010101 
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int number;
+    cin>>number;
+    cout << (number & (number-1)==0) && (number & BINARYHOLDER1)==number;
+
+    return 0;
+}
