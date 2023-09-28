d66669f391e696418d59ea10c62e26455d0cd4fd
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-09-28 23:12:18 +0300

    feat : solved subsets

---
+++ SubsetsNumerical.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> subsets(vector<int>& nums) {
+    unordered_set<vector<int>> sol;
+    for(int i = 0 ; i<1<<nums.size(); i+=1) {
+        int copy = i;
+        vector<int> indiv ={};
+        for(auto x : nums) {
+            if(copy&1){
+                indiv.push_back(x);
+            }
+            copy>>=1;
+        }
+        sol.insert(indiv);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> nums = {1,2,3};
+    int counter = 0;
+    auto sol = subsets(nums);
+    for(auto row : sol) {
+        cout<<"This is: "<<counter<<" index"<<endl;
+        for(auto number : row) {
+            cout<<"\t"<<number;
+        }
+        counter+=1;
+        cout<<endl;
+    }
+
+    
+
+    return 0;
+}
