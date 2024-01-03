2e2a973252bacbe06346d994a7128aa3c8ad93ab
lihka1202 <mnakhilwork@gmail.com>
2024-01-03 14:16:50 +0800

    debug : check if multiple account commits works

--- powerof4.cpp
+++ powerof4.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define BINARYHOLDER1 1431655765
 //this is 1010101010101010101010101010101 
 
+// Sending this with multiple git profiles and local hits
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
